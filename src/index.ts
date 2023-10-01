7498f733138f46caa9587ad503e69874e1bb0e9d
yashbidasaria <bidasariayash@gmail.com>
2023-10-01 18:30:46 -0400

    Add joke api call

--- src/index.ts
+++ src/index.ts
@@ -3,22 +3,37 @@ import path from "path"
 import fs from "fs/promises"
 import url from "url"
 
+interface Joke {
+    id: string
+    joke: string
+    status: number
+}
 async function requestListener(req: IncomingMessage, res: ServerResponse) {
     const parsedUrl = url.parse(req.url || "")
 
-    let data
+    let data = ""
     let statusCode = 200
     try {
         let pathname = parsedUrl.pathname
         if (pathname == "/") pathname = "/index";
         const filepath = path.join(__dirname, `static${pathname}.html`)
-        data = await fs.readFile(filepath)
+        data = await fs.readFile(filepath, "utf-8")
 
     } catch {
-        data = await fs.readFile(path.join(__dirname, "static/404.html"))
+        data = await fs.readFile(path.join(__dirname, "static/404.html"), "utf-8")
         statusCode = 404
     }
     
+    if (parsedUrl.pathname == "/dad-joke") {
+        const response = await fetch("https://icanhazdadjoke.com/", {
+            headers: {
+                accept: "application/json",
+                "user-agent": "NodeJS Server"
+            }
+        })
+        const joke: Joke = await response.json();
+        data = data.replace(/{{joke}}/gm, joke.joke)
+    }
     res.writeHead(
         statusCode, {
             "Content-type": "text/html",
